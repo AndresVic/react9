c8036a28cceafbcd84a20f5d05dc996d22dd0065
Andres Victoriano <al221811724@gmail.com>
2021-05-22 18:18:52 -0500

    Se creo un index, se agregaron 3 imagenes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@
 import React from "react"
 import reactDom from "react-dom"
 
-import img_team from "./img/img_team.png"
-import archivo_1 from "./files/horario.pdf"
+import img_cuervos from "./img/cuervos.jpg"
+import img_utvt_logo from "./img/utvt-logo.png"
 
 const contenido = <div>
-    <img src={img_team} alt="Logo de un Equipo" />
-    <img src="https://i.pinimg.com/originals/e2/ab/0a/e2ab0a2c07931b4c582dec61cc364abe.jpg" alt="Logo de un Equipo" />
+    <img src={img_cuervos} alt="Logo de los Cuervos UTVT" /> 
+    <img src={img_utvt_logo} alt="Logo de UTVT" />
     <br/>
-    <a href={archivo_1} rel="noopener noreferrer" target="_blank">Archivo PDF</a>
 </div>
 
 reactDom.render(contenido, document.getElementById('root'))
